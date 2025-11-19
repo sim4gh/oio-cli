f1669667f42c44ff56e41ec8bb1c5f9f9575ed0c
sim4r4 <me@miguelaramis.com>
2025-11-18 21:39:27 -0800

    Bump version to 1.0.1

--- oio.rb
+++ oio.rb
@@ -1,7 +1,7 @@
 class Oio < Formula
   desc "CLI for managing notes and ephemeral shorts with authenticated access"
   homepage "https://github.com/sim4gh/oio-cli"
-  url "https://github.com/sim4gh/oio-cli/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/sim4gh/oio-cli/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "ISC"
 
@@ -19,6 +19,6 @@ class Oio < Formula
   end
 
   test do
-    assert_match "1.0.0", shell_output("#{bin}/oio --version")
+    assert_match "1.0.1", shell_output("#{bin}/oio --version")
   end
 end
